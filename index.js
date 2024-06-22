0dca4d3df92285cc71c909fc17f8caf0f516df5d
hemanth <hemanthgirimath888@gmail.com>
2024-06-22 23:07:26 +0530

    Implemented Completed RAG using Langchain and Gemini api

--- index.js
+++ index.js
@@ -1,41 +1,33 @@
 import express, { query } from 'express'
-const app = express()
-const port = 4001
 import 'dotenv/config'
 import { MongoClient } from 'mongodb'
 import { CharacterTextSplitter } from "@langchain/textsplitters";
-// app.use(express.json());
-// app.use(cookieParser());
-
+import { MongoDBAtlasVectorSearch} from "@langchain/mongodb";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence, RunnablePassthrough } from "@langchain/core/runnables";
+import { formatDocumentsAsString } from "langchain/util/document";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import * as fs from "fs/promises";
-import {
-  Gemini,
-  Document,
-  VectorStoreIndex,
-  Settings,
-  GEMINI_MODEL,
-  GeminiEmbedding,
-  GEMINI_EMBEDDING_MODEL,
-} from "llamaindex";
-
-
+import {Gemini,Settings,GEMINI_MODEL,GeminiEmbedding,GEMINI_EMBEDDING_MODEL} from "llamaindex";
+import { GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI } from "@langchain/google-genai";
+  const app = express()
+  const port = 4001
 // setting up DB/Connection
 const url = "mongodb+srv://HemanthGirimath:bJhz4zkEnXDoKvVI@geminiragllm.oe9topz.mongodb.net/?retryWrites=true&w=majority&appName=GeminiRagLlm"
 const Client  = new MongoClient(url);
+let vectoreStore 
 
 async function ConnectToMongoDb(){
   try {
     await Client.connect();
     console.log("Successfully connected to Atlas");
-    main();
+    run()
 
     } catch (err) {
         console.log(err.stack);
     }
-    finally {
-        await Client.close();
-}
-
 }
 Settings.llm = new Gemini({
   model: GEMINI_MODEL.GEMINI_PRO_LATEST,
@@ -45,74 +37,77 @@ Settings.embedModel = new GeminiEmbedding({
   model:GEMINI_EMBEDDING_MODEL.TEXT_EMBEDDING_004
 });
 
-async function main() {
-  const googleApiKey = process.env.GOOGLE_API_KEY;
-  if (!googleApiKey) {
-    throw new Error('Set Google API Key in GOOGLE_API_KEY env variable');
+async function run(){
+  await Client.connect();
+  try{
+    const database = Client.db("langchain_db");
+    const collection = database.collection("test");
+    const dbConfig = {  
+      collection: collection,
+      indexName: "vector_index",
+      textKey: "text", 
+      embeddingKey: "embedding",
+    };
+    await collection.deleteMany({});
+  const path = "node_modules/llamaindex/examples/abramov.txt"
+  const eassay = await fs.readFile(path,'utf-8')
+  const textsplitters = new RecursiveCharacterTextSplitter({
+    separators:"\n\n",
+    chunkSize:1000,
+    chunkOverlap:200,
+  });
+  const docs = await textsplitters.createDocuments([eassay]);
+  const embeddings = new GoogleGenerativeAIEmbeddings({
+    model: "embedding-001", // 768 dimensions
+   apiKey:process.env.GOOGLE_API_KEY
+  });
+  vectoreStore = await MongoDBAtlasVectorSearch.fromDocuments(docs,embeddings,dbConfig);
+  QueryMongo();
   }
-//load datat fron node
-  const path = "node_modules/llamaindex/examples/abramov.txt";
-  const essay = await fs.readFile(path, "utf-8");
-//create document object with essay
-const document = new Document({ text: essay, id_: path});
-const index = await VectorStoreIndex.fromDocuments([document]);
-await Client.connect();
-const db = Client.db("langchain_db");
-const col = db.collection("VectorStore");
-// const insertData = col.insertOne(index)
-// console.log("inserted the data")
-// const retiver = index.asRetriever();
-// retiver.topK = 6;
-// console.log("reterivers relavent info",retiver)
-// const queryEngine = index.asQueryEngine({retiver});
-// const  response  = await queryEngine.query({query: "when was the first job for the author and what was the salary?"})
-const embedding = new GeminiEmbedding();
-const embeddingResponse = await embedding.getTextEmbedding("when was authors first job?")
-
-console.log(embeddingResponse)
-QueryMongdb(embeddingResponse)
+  catch(error){
+    console.log(error)
+  }
+  console.log("Waiting for initial sync...");
+  await new Promise(resolve => setTimeout(() => {
+  resolve();
+}, 10000));
 }
 
-
-
-async function QueryMongdb(queryText) {
-    try {
-        const database = Client.db("langchain_db");
-        const coll = database.collection("VectorStore");
-        // define pipeline
-        const agg = [
-            {
-              '$vectorSearch': {
-                'index': 'vector_index', 
-                'path': 'embeddings', 
-                'queryVector': queryText, 
-                'numCandidates': 150, 
-                'limit': 10
-              }
-            }, {
-              '$project': {
-                '_id': 0, 
-                'plot': 1, 
-                'title': 1, 
-                'score': {
-                  '$meta': 'vectorSearchScore'
-                }
-              }
-            }
-          ];
-        // run pipeline
-        const result = coll.aggregate(agg);
-
-        // print results
-      //  await result.forEach((doc) => console.dir(JSON.stringify(doc)));
-      console.log(result)
-
-    } 
-    catch(error){
-      console.log(error)
-    }
+async function QueryMongo(){
+  if (!vectoreStore) {
+    console.error("Vector store is not initialized. Run the initialization function first.");
+    return;
   }
+  const retriver = vectoreStore.asRetriever();
+  const prompt =
+  PromptTemplate.fromTemplate(`Answer the question based on the following context:
+  {context}
+  Question: {question}`);
+const model = new ChatGoogleGenerativeAI({
+  apiKey:process.env.GOOGLE_API_KEY,
+  model:"gemini-pro"
+});
+const chain = RunnableSequence.from([
+  {
+    context: retriver.pipe(formatDocumentsAsString),
+    question: new RunnablePassthrough(),
+  },
+  prompt,
+  model,
+  new StringOutputParser(),
+]);
+const question = "what was the authors sakary on his first job";  
+const answer = await chain.invoke(question);
+console.log("Question: " + question);
+console.log("Answer: " + answer);
 
+const retrievedResults = await retriver.getRelevantDocuments(question);
+const documents = retrievedResults.map((documents => ({
+  pageContent: documents.pageContent,
+})))
+console.log("\nSource documents:\n" + JSON.stringify(documents, 1, 2))
+
+}
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
